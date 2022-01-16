fae3e533835f6a9a3459cc55730527a66b57e23c
Jagdish2022 <94558144+Jagdish2022@users.noreply.github.com>
2022-01-16 16:51:38 +0530

    first_file

---
+++ practise_dictionary.py
@@ -0,0 +1,45 @@
+# dict = {
+# 1: 'Jagdish',
+# 2:  'Ramesh',
+# 3:  'Rohit'
+# }
+
+# print(dict)
+# print(dict[3])
+# print(dict.get(2))
+
+# for key, value in dict.items():
+#     if key == 1:
+#         print(value)
+
+dics = {
+'Sunil Kumar': {
+    "Father_name": 'Ramesh Kumar',
+    "Age":  35,
+    "Gender": "M"
+},
+'Kamal Kumar': {
+    "Father_name": 'Bimlesh Kumar',
+    "Age":  22,
+    "Gender": "M"
+},
+
+'Vinod Kumar':{
+    "Father_name": 'Kala Kumar',
+    "Age":  32,
+    "Gender": "M"
+    }
+}
+
+
+enter_name = str(input('Please enter the employee name '))
+
+try:
+    print(f"{enter_name} father is {dics[enter_name]['Father_name']}")
+except:
+    print("Sorry ! Please enter the valid name ")
+
+
+print(dics['Sunil Kumar'])
+# print(dics.get(5)['Name'])
+# print(dics[5]['Gender'])
